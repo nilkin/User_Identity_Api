a85398c9fc74cc3543573ebee930d0e337c97f47
Your Name <ilkinnazarli@gmail.com>
2021-05-02 02:09:09 +0400

    create identity api with login add to project sqlite

---
+++ Domain/Entities/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+   public class About
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string PhotoURL { get; set; }
+    }
+}
---
+++ Persistence/Migrations/20210501212402_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Text = table.Column<string>(type: "TEXT", nullable: true),
+                    PhotoURL = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+        }
+    }
+}

0c4fd2d5e12f4260f6321d4d171ef14d8e42bc41
Your Name <ilkinnazarli@gmail.com>
2021-05-03 02:16:31 +0400

    add AboutsController get abouts and single about by id end points

---
+++ API/Controllers/AboutsController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AboutsController :BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AboutsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<About>>>GetAbouts()
+        {
+            return await _context.Abouts.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<About>> GetAbout(Guid Id)
+        {
+            return await _context.Abouts.FindAsync(Id);
+        }
+    }
+}
